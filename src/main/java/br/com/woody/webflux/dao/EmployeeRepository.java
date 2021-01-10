6ac7a516fe8fcace3b6c1202b1e3fb7474473d98
thiagodelbusso <thiago.delbusso@gmail.com>
2021-01-10 16:31:37 -0300

    FindAll with pagination

--- src/main/java/br/com/woody/webflux/dao/EmployeeRepository.java
+++ src/main/java/br/com/woody/webflux/dao/EmployeeRepository.java
@@ -1,11 +1,16 @@
 package br.com.woody.webflux.dao;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.woody.webflux.model.Employee;
+import reactor.core.publisher.Flux;
 
 @Repository
-public interface EmployeeRepository extends R2dbcRepository<Employee, Integer> {
-
+public interface EmployeeRepository extends R2dbcRepository<Employee, Integer> { 
+	
+	@Query("SELECT * FROM employee OFFSET :offset LIMIT :limit")
+	Flux<Employee> findAll(int offset, int limit);
+	
 }
--- src/main/java/br/com/woody/webflux/service/EmployeeService.java
+++ src/main/java/br/com/woody/webflux/service/EmployeeService.java
@@ -1,29 +1,37 @@
 package br.com.woody.webflux.service;
 
-import static br.com.woody.webflux.query.Employee.SELECT_ALL_JOIN_DEPARTMENT;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Service;
 
-import br.com.woody.webflux.mapping.EmployeeMapping;
+import br.com.woody.webflux.dao.DepartmentRepository;
+import br.com.woody.webflux.dao.EmployeeRepository;
 import br.com.woody.webflux.model.Employee;
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Log4j2
 @Service
 public class EmployeeService {
 	
 	@Autowired
-    private DatabaseClient databaseClient;
+    private EmployeeRepository employeeDao;
+	
+	@Autowired
+	private DepartmentRepository departmentDao;
 	
 	public Flux<Employee> listarTodos() {
 		log.info("Montando fluxo de Employe...");
-		return this.databaseClient
-				   .sql(SELECT_ALL_JOIN_DEPARTMENT)
-				   .map(EmployeeMapping.MAPPING_FUNCTION)
-				   .all();
+		
+		int offset = 0;
+		int limit = 100;
+		return employeeDao.findAll(offset, limit)
+						  .flatMap(emp -> Mono.just(emp)
+								   .zipWith(departmentDao.findById("0001"))
+								   .map(t -> {
+									   emp.setDepartamento(t.getT2());
+									   return emp;
+								   }));
 	}
 	
 }
